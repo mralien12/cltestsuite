350ec28849931011b1b61b8938c3a26ca3f70fcd
Sungtai Kim <st8018@alticast.com>
2018-01-04 16:24:55 +0900

    [SF] Modify Sectionfilter test case.

--- app/src/androidTest/java/com/alticast/taurus/testsuite/mpeg/SectionFilterTest.java
+++ app/src/androidTest/java/com/alticast/taurus/testsuite/mpeg/SectionFilterTest.java
@@ -20,9 +20,14 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.util.concurrent.TimeUnit;
+
 import af.channel.Channel;
 import af.channel.ChannelManager;
+import af.media.Tuner;
+import af.media.TunerFactory;
 import af.mpeg.section.ConnectionLostException;
+import af.mpeg.section.EndOfFilteringEvent;
 import af.mpeg.section.FilterResourceException;
 import af.mpeg.section.FilteringInterruptedException;
 import af.mpeg.section.IllegalFilterDefinitionException;
@@ -37,6 +42,7 @@ import af.mpeg.section.TimeOutEvent;
 import af.resource.NoAvailableResourceException;
 import af.resource.ResourceClient;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 /**
@@ -48,20 +54,23 @@ public class SectionFilterTest {
 
     private final static int NUMBER_OF_SECTION_FILTER = 1;
     private final int PMT_PID = 0x13EC;
+    private final int PAT_PID= 0x00;
     private final int PAT_TABLE_ID = 0x00;
     private final int PMT_TABLE_ID = 0x02;
 
-    private Channel[] channels;
-    private Channel currentChannel;
+    private static  Channel[] channels;
+    private static Channel currentChannel;
     private static SectionFilterGroup sectionFilterGroup;
+    private static boolean onTuneEvent;
+    private static Tuner singleTuner;
+    private static int sourceId;
+
     @BeforeClass
     public static void setUp(){
+        TLog.i("SetUp", "SetUp SectionfilterGroup #" + NUMBER_OF_SECTION_FILTER );
         sectionFilterGroup = new SectionFilterGroup(NUMBER_OF_SECTION_FILTER);
-    }
 
-   @Test(timeout = 300000)      // Set timeout for test to 5 minutes
-    public void sectionFilterTest(){
-
+        TLog.i("Setup", "Check Channels" );
         if (channels == null) {
             channels = ChannelManager.getInstance().getChannelList(ChannelManager.CHANNEL_LIST_ALL);
         }
@@ -69,25 +78,98 @@ public class SectionFilterTest {
         assertThat("Length of channel list must be greater than 0", channels.length > 0);
         currentChannel = channels[0];
 
+        singleTuner = TunerFactory.getInstance().createTuner(new ResourceClient() {
+            @Override
+            public String getName() {
+                return null;
+            }
+
+            @Override
+            public String getDescription() {
+                return null;
+            }
+        });
+
+        singleTuner.setTuneEventListener(new Tuner.TunerEventListener() {
+            @Override
+            public void onTuningEvent(int i, String s) {
+                onTuneEvent = true;
+                TLog.i(this, "singleTest: onTuningEvent [ " + i + "] [" + s +"]" );
+                if (i == 1) {
+                    sourceId = singleTuner.getSourceId();
+                }
+            }
+        });
+
+        onTuneEvent = false;
+        try {
+            assertThat("Is single tuner tune successfully?", singleTuner.tune(currentChannel.getUri()), is(true));
+        } catch (NoAvailableResourceException e) {
+            e.printStackTrace();
+        }
+
+        /* Wait few miniseconds for invoking callback */
+        try {
+            TimeUnit.MILLISECONDS.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        assertThat("Is onTuningEvent() callback invoked?", onTuneEvent, is(true));
+    }
+
+   @Test(timeout = 30000)      // Set timeout for test to 30 Sec
+    public void sectionFilterTest(){
+        TLog.i(this, "Start SF test 30 Sec" );
+
         TableSectionFilter tableSectionFilter = sectionFilterGroup.newTableSectionFilter();
 
         /* Set timeout for section filter to 10s */
-        tableSectionFilter.setTimeOut(10000);
+        //tableSectionFilter.setTimeOut(20000);
 
         SectionFilterListener myListener = new SectionFilterListener() {
             @Override
             public void onSectionFilterEvent(SectionFilterEvent event) {
-                TLog.i(this, "onSectionFilterEvent()");
+                TLog.i(this, "onSectionFilterEvent() [" + event + "]");
                 if (event instanceof TimeOutEvent){
                     TLog.i(this, "Section filter operations time out");
                 } else if(event instanceof IncompleteFilteringEvent) {
                     TLog.i(this, "Filter parameters is incorrect");
                 } else if (event instanceof SectionAvailableEvent) {
-                    TLog.i(this, "Section filter operations is successful");
-                    TableSectionFilter tblSectionFilter = (TableSectionFilter) event.getSource();
+                    TLog.i(this, "Section filter operations is successful. do nothing!!");
+                } else if (event instanceof EndOfFilteringEvent) {
+                    TLog.i(this, "Section filter operations is EndOfFilteringEvent");
+                    TableSectionFilter recievesf = (TableSectionFilter) event.getSource();
+
                     try {
-                        Section[] sections = tblSectionFilter.getSections();
+                        Section currentsection[] = recievesf.getSections();
+
+                        int seclen = currentsection.length;
+                        try {
+                            for(int j = 0 ; j < seclen ; j ++) {
+                                byte[] sectiondata = currentsection[j].getData();
+                                int dataLen = sectiondata.length;
+                                TLog.i(this, "section data [" + dataLen + "]");
+                                if (dataLen >= 20) {
+                                    dataLen = 20;
+                                    TLog.i(this, "section data [" + dataLen + "]. just print out 20 bytes.");
+                                }
+                                StringBuffer result = new StringBuffer();
+                                result.append("Section Data [" + j + "]");
+                                for (int i = 0; i < dataLen; i++) {
+                                    result.append(" [" + String.format("%02x", sectiondata[i]) + "]");
+                                }
+                                TLog.i(this, result.toString());
+                                currentsection[j].setEmpty();
+                            }
+                        } catch (Exception e1) {
+                            TLog.i(this, "Exception");
+                            e1.printStackTrace();
+                        }
+                        //don't stop any sectionfilter.
+
                     } catch (FilteringInterruptedException e) {
+                        TLog.i(this, "FilteringInterruptedException");
                         e.printStackTrace();
                     }
                 }
@@ -95,6 +177,9 @@ public class SectionFilterTest {
         };
 
         tableSectionFilter.setSectionFilterListener(myListener);
+
+       TLog.i(this, "setSourceId [" +sourceId +"]" );
+       sectionFilterGroup.setSourceId(sourceId);
 
         try {
             TLog.i(this, "Set the parameters on the filter");
