13f54816cedd9fea6722029b153e4c0488f46d99
Tran Khanh Hoa <tk.hoa@alticast.com>
2018-03-07 16:57:53 +0700

    Initial commit
    Signed-off-by: Tran Khanh Hoa <tk.hoa@alticast.com>

---
+++ AndroidTVTestApp/app/src/main/java/alticast/com/androidtvtestapp/RecordingActivity.java
@@ -0,0 +1,13 @@
+package alticast.com.androidtvtestapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RecordingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recording);
+    }
+}
---
+++ AndroidTVTestApp/app/src/main/java/alticast/com/androidtvtestapp/TestCase.java
@@ -0,0 +1,41 @@
+package alticast.com.androidtvtestapp;
+
+/*
+ *  Copyright (c) 2018 Alticast Corp.
+ *  All rights reserved. http://www.alticast.com/
+ *
+ *  This software is the confidential and proprietary information of
+ *  Alticast Corp. ("Confidential Information"). You shall not
+ *  disclose such Confidential Information and shall use it only in
+ *  accordance with the terms of the license agreement you entered into
+ *  with Alticast.
+ */
+public class TestCase {
+    public static final int SCAN_TEST = 0;
+    public static final int TUNER_TEST = 1;
+    public static final int SECTION_FILTER_TEST = 2;
+    public static final int RECORDING_TEST = 3;
+    public static final int BLOCKING_PORT_TEST = 4;
+
+    public static final String TEST_RESULT = "result";
+    public static final String TC_NAME = "TC_name";
+
+    private String name;
+    private boolean testResult;
+
+    public boolean getTestResult() {
+        return testResult;
+    }
+
+    public void setTestResult(boolean testResult) {
+        this.testResult = testResult;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
